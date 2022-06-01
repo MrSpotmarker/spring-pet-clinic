a2e511cda2b20697e91df46cf8e514af6254f75d
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-06-01 13:22:51 +0200

    adding POJOs

---
+++ src/main/java/at/jhofer/demo/model/Person.java
@@ -0,0 +1,38 @@
+/*********************************************************************
+ * The Initial Developer of the content of this file is NETCONOMY.
+ * All portions of the code written by NETCONOMY are property of
+ * NETCONOMY. All Rights Reserved.
+ *
+ * NETCONOMY Software & Consulting GmbH
+ * Hilmgasse 4, A-8010 Graz (Austria)
+ * FN 204360 f, Landesgericht fuer ZRS Graz
+ * Tel: +43 (316) 815 544
+ * Fax: +43 (316) 815544-99
+ * www.netconomy.net
+ *
+ * (c) 2022 by NETCONOMY Software & Consulting GmbH
+ *********************************************************************/
+
+package at.jhofer.demo.model;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
--- src/test/java/at/jhofer/demo/SpringPetClinicApplicationTests.java
+++ src/test/java/at/jhofer/demo/SpringPetClinicApplicationTests.java
@@ -1,13 +1,8 @@
 package at.jhofer.demo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringPetClinicApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
