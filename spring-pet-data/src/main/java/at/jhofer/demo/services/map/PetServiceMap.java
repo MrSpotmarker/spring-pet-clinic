49dfcd1b1eb4607871bb7403c31930ce843ffea6
Joachim Hofer <joachim.hofer@gmail.com>
2022-06-07 23:55:32 +0200

    impl map based services. closes #6

---
+++ spring-pet-data/src/main/java/at/jhofer/demo/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package at.jhofer.demo.services.map;
+
+import at.jhofer.demo.model.Pet;
+import at.jhofer.demo.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
