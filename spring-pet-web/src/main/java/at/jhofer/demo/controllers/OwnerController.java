4586c345fb19fd1edc1fd15c5efbefc124531270
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-06-08 08:57:07 +0200

    List all Owners on Owner index page. Closes #17

---
+++ spring-pet-web/src/main/java/at/jhofer/demo/controllers/OwnerController.java
@@ -0,0 +1,29 @@
+/*********************************************************************
+ * The Initial Developer of the content of this file is NETCONOMY.
+ * All portions of the code written by NETCONOMY are property of
+ * NETCONOMY. All Rights Reserved.
+ *
+ * NETCONOMY Software & Consulting GmbH
+ * Hilmgasse 4, A-8010 Graz (Austria)
+ * FN 204360 f, Landesgericht fuer ZRS Graz
+ * Tel: +43 (316) 815 544
+ * Fax: +43 (316) 815544-99
+ * www.netconomy.net
+ *
+ * (c) 2022 by NETCONOMY Software & Consulting GmbH
+ *********************************************************************/
+
+package at.jhofer.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String ownerList() {
+        return "owners/index";
+    }
+}
