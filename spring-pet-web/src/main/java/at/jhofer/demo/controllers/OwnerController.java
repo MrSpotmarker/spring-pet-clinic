8c19dae50e4172b5f9a94c285d89abb0c24d0531
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-06-10 07:15:32 +0200

    Create Owner index page and Controller. Closes #14

--- spring-pet-web/src/main/java/at/jhofer/demo/controllers/OwnerController.java
+++ spring-pet-web/src/main/java/at/jhofer/demo/controllers/OwnerController.java
@@ -15,15 +15,24 @@
 
 package at.jhofer.demo.controllers;
 
+import at.jhofer.demo.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String ownerList() {
+    public String ownerList(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
