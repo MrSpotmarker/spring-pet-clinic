e33c17c385f292d8f671673f8f7c0a76baf97ff8
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-06-10 07:22:24 +0200

    List all vets on Vet index page. Closes #16

--- spring-pet-web/src/main/java/at/jhofer/demo/controllers/VetController.java
+++ spring-pet-web/src/main/java/at/jhofer/demo/controllers/VetController.java
@@ -15,14 +15,23 @@
 
 package at.jhofer.demo.controllers;
 
+import at.jhofer.demo.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "vets/index", "vets/index.html"})
-    public String vetList() {
+    public String vetList(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
