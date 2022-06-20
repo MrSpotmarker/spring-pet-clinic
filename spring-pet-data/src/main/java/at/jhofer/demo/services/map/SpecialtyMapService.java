31a78307cc44f849f159368fc0e7af7a3c96d886
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-06-20 22:34:43 +0200

    Create Specialty Map Service Impl. Closes #32

---
+++ spring-pet-data/src/main/java/at/jhofer/demo/services/map/SpecialtyMapService.java
@@ -0,0 +1,51 @@
+/*********************************************************************
+ * The Initial Developer of the content of this file is NETCONOMY.
+ * All portions of the code written by NETCONOMY are property of
+ * NETCONOMY. All Rights Reserved.
+ *
+ * NETCONOMY Software & Consulting GmbH
+ * Hilmgasse 4, A-8010 Graz (Austria)
+ * FN 204360 f, Landesgericht fuer ZRS Graz
+ * Tel: +43 (316) 815 544
+ * Fax: +43 (316) 815544-99
+ * www.netconomy.net
+ *
+ * (c) 2022 by NETCONOMY Software & Consulting GmbH
+ *********************************************************************/
+
+package at.jhofer.demo.services.map;
+
+import at.jhofer.demo.model.Specialty;
+import at.jhofer.demo.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
