bd059c1aef16862d135ddbe0e81d60725126986c
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-07-21 22:21:31 +0200

    Convert Vets to JPA. Closes #38

--- spring-pet-data/src/main/java/at/jhofer/demo/model/Specialty.java
+++ spring-pet-data/src/main/java/at/jhofer/demo/model/Specialty.java
@@ -15,7 +15,14 @@
 
 package at.jhofer.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
