c8afe7d61731d542ffd15356d8f770dcee8a7e7c
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-07-21 22:53:43 +0200

    Create Visit entity. Closes #39

--- spring-pet-data/src/main/java/at/jhofer/demo/model/Visit.java
+++ spring-pet-data/src/main/java/at/jhofer/demo/model/Visit.java
@@ -17,9 +17,23 @@ package at.jhofer.demo.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String desciption;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
