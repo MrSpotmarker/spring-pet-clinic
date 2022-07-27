c9fd7b4e8a6ab8eeb361e0e7efb7f1453fea4829
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-07-27 07:22:49 +0200

    Create Spring Data JPA Service for Vets. Closes #42

---
+++ spring-pet-data/src/main/java/at/jhofer/demo/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,63 @@
+/*********************************************************************
+ * The Initial Developer of the content of this file is NETCONOMY.
+ * All portions of the code written by NETCONOMY are property of
+ * NETCONOMY. All Rights Reserved.
+ *
+ * NETCONOMY Software & Consulting GmbH
+ * Hilmgasse 4, A-8010 Graz (Austria)
+ * FN 204360 f, Landesgericht fuer ZRS Graz
+ * Tel: +43 (316) 815 544
+ * Fax: +43 (316) 815544-99
+ * www.netconomy.net
+ *
+ * (c) 2022 by NETCONOMY Software & Consulting GmbH
+ *********************************************************************/
+
+package at.jhofer.demo.services.springdatajpa;
+
+import at.jhofer.demo.model.Vet;
+import at.jhofer.demo.model.repositories.VetRepository;
+import at.jhofer.demo.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
