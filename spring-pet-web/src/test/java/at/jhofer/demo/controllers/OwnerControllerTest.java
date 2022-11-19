c37d371182daffaba6e69e9847b8f4305dc91ed3
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-11-19 17:25:02 +0100

    Display Owner Record (HTML). Closes #54

--- spring-pet-web/src/test/java/at/jhofer/demo/controllers/OwnerControllerTest.java
+++ spring-pet-web/src/test/java/at/jhofer/demo/controllers/OwnerControllerTest.java
@@ -14,7 +14,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -71,5 +74,15 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners/find"))
             .andExpect(status().isOk())
             .andExpect(view().name("notimplemented"));
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/1"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("owners/ownerDetails"))
+            .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
