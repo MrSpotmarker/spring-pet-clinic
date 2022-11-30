2b87404fa605105014ed8469677275612513be6c
MrSpotmarker <TT4c5vfdBdXfVArsctJ3QC7U55bG>
2022-12-01 00:13:52 +0100

    Create Spring MockMVC Tests for Visit Controller. Closes #66

--- spring-pet-web/src/main/java/at/jhofer/demo/controllers/VisitController.java
+++ spring-pet-web/src/main/java/at/jhofer/demo/controllers/VisitController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -30,8 +32,16 @@ public class VisitController {
 
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder) {
+    public void dataBinder(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(LocalDate.parse(text));
+            }
+        });
+
     }
 
     /**
